209c7ac71f403af68ba95e2efb7293ea3cec7035
Linh Ly <lly23@live.unc.edu>
2018-10-10 12:37:28 -0400

    changed timeline colors and ad in some fade to the title page

--- js/script.js
+++ js/script.js
@@ -2,16 +2,31 @@ $(document).ready(function() {
     // var scene = document.getElementById('scene');
     // var parallaxInstance = new Parallax(scene);
 
-    // $.jInvertScroll(['.scroll']);
     $('.timeline').timelify({
-        // animation types
-        animLeft: "bounceInLeft",
-        animRight: "bounceInRight",
-        animCenter: "bounceInUp",
-        // animation speed
-        animSpeed: 300,
-        // trigger position in pixels
-        offset: 150
-      });
-      
+      animLeft: "fadeInLeft",
+      animCenter: "fadeInUp",
+      animRight: "fadeInRight",
+      animSpeed: 600,
+      offset: 150
+    });
+
+    $(window).scroll(function () {
+      var scrollTop = $(window).scrollTop();
+      var height = $(window).height();
+      var topOfTimeline = $('#content').offset().top;
+  
+      $('#title').css({
+          'opacity': ((height - scrollTop) / height)
+      }); 
+
+      if($(window).scrollTop() > topOfTimeline) { //scrolled past the other div?
+        // $('#title').css('display', 'none');
+        // $('#title').hide();
+        // $('#content').css('margin-top', 0);
+      } else if ($(window).scrollTop() == topOfTimeline) {
+        // $('#title').css('display', 'flex');
+        // $('#title').show();
+        $('#content').css('margin-top', '100vh');
+      }
+    });
 });
