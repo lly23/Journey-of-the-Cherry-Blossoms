f7a9825d7985feebffd19c809f08fb7fa3b9070b
Linh Ly <lly23@live.unc.edu>
2018-10-10 23:52:29 -0400

    changed timeline content and positioning

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    // var scene = document.getElementById('scene');
+    // var scene = document.getElementById('#title');
     // var parallaxInstance = new Parallax(scene);
 
     $('.timeline').timelify({
@@ -11,6 +11,10 @@ $(document).ready(function() {
     });
 
     $(window).scroll(function () {
+      // sakura drop
+
+
+
       var scrollTop = $(window).scrollTop();
       var height = $(window).height();
       var topOfTimeline = $('#content').offset().top;
@@ -19,11 +23,27 @@ $(document).ready(function() {
           'opacity': ((height - scrollTop) / height)
       }); 
 
+      if ($(window).scrollTop() > ($('#content').offset().top)) {
+        console.log('window: ' + $(window).scrollTop());
+        console.log('panel one: ' + $('#content').offset().top);
+        $('#one').addClass('changed');
+      } 
+
+      if ($(window).scrollTop() > ($('#content').offset().bottom) / 2) {
+        console.log('window: ' + $(window).scrollTop());
+        console.log('half of panel one: ' + $('#one').offset().bottom);
+        $('#two').addClass('changed');
+      }
+
+      // if ($(window).scrollTop() > $('#panelOne').offset().top) {
+      //   $('#one').fadeTo("slow" , 0.5);
+      // }
+
       if($(window).scrollTop() > topOfTimeline) { //scrolled past the other div?
         // $('#title').css('display', 'none');
         // $('#title').hide();
         // $('#content').css('margin-top', 0);
-      } else if ($(window).scrollTop() == topOfTimeline) {
+      } else if ($(window).scrollTop() < topOfTimeline) {
         // $('#title').css('display', 'flex');
         // $('#title').show();
         // $('#content').css('margin-top', '100vh');
