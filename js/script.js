d7486e2a1ae4d89e0829d338f4a591ee42ce4351
Linh Ly <lly23@live.unc.edu>
2018-10-11 22:34:16 -0400

    added parallax idea to layout and background image

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-    // var scene = document.getElementById('#title');
-    // var parallaxInstance = new Parallax(scene);
+    // waypoints plugin
+    var waypoint = new Waypoint({
+      element: document.getElementById('panelOne'),
+      handler: function() {
+        notify('Basic waypoint triggered')
+      }
+    })
 
+    // timelify plugin
     $('.timeline').timelify({
       animLeft: "fadeInLeft",
       animCenter: "fadeInUp",
@@ -14,39 +20,28 @@ $(document).ready(function() {
       // sakura drop
 
 
-
+      // fade when scrolling down
       var scrollTop = $(window).scrollTop();
       var height = $(window).height();
-      var topOfTimeline = $('#content').offset().top;
   
       $('#title').css({
           'opacity': ((height - scrollTop) / height)
-      }); 
+      });    
 
+      //changes color of scrolled date boxes
       if ($(window).scrollTop() > ($('#content').offset().top)) {
         console.log('window: ' + $(window).scrollTop());
         console.log('panel one: ' + $('#content').offset().top);
         $('#one').addClass('changed');
       } 
 
-      if ($(window).scrollTop() > ($('#content').offset().bottom) / 2) {
+      // $('#one').fadeOut('slow', 'linear');
+
+      if ($(window).scrollTop() > ($('#one').height()) / 2) {
         console.log('window: ' + $(window).scrollTop());
-        console.log('half of panel one: ' + $('#one').offset().bottom);
+        console.log('half of panel one: ' + $('#one').height());
         $('#two').addClass('changed');
       }
 
-      // if ($(window).scrollTop() > $('#panelOne').offset().top) {
-      //   $('#one').fadeTo("slow" , 0.5);
-      // }
-
-      if($(window).scrollTop() > topOfTimeline) { //scrolled past the other div?
-        // $('#title').css('display', 'none');
-        // $('#title').hide();
-        // $('#content').css('margin-top', 0);
-      } else if ($(window).scrollTop() < topOfTimeline) {
-        // $('#title').css('display', 'flex');
-        // $('#title').show();
-        // $('#content').css('margin-top', '100vh');
-      }
     });
 });
